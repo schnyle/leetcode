da911c95dec121bfa8a411ad8f8945b22375ed6c
kyle <kylesch115@gmail.com>
2026-03-13 23:01:06 -0600

    0002_add_two_numbers

---
+++ 0002_add_two_numbers.c
@@ -0,0 +1,106 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+ * order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example 1:
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ *
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ *
+ * Constraints:
+ *
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+  int val;
+  struct ListNode *next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
+{
+  struct ListNode dummy = {0, NULL};
+  struct ListNode *node = &dummy;
+  int carry = 0;
+
+  while (l1 != NULL || l2 != NULL || carry)
+  {
+    int sum = carry;
+    carry = 0;
+
+    if (l1)
+    {
+      sum += l1->val;
+      l1 = l1->next;
+    }
+    if (l2)
+    {
+      sum += l2->val;
+      l2 = l2->next;
+    }
+
+    if (sum >= 10)
+    {
+      sum -= 10;
+      carry = 1;
+    }
+
+    struct ListNode *new_node = malloc(sizeof(struct ListNode));
+    new_node->val = sum;
+    new_node->next = NULL;
+    node->next = new_node;
+    node = new_node;
+  }
+
+  return dummy.next;
+}
+
+// testing
+int main()
+{
+  // example 1
+  struct ListNode *l1 = malloc(sizeof(struct ListNode));
+  l1->val = 2;
+  l1->next = malloc(sizeof(struct ListNode));
+  l1->next->val = 4;
+  l1->next->next = malloc(sizeof(struct ListNode));
+  l1->next->next->val = 3;
+
+  struct ListNode *l2 = malloc(sizeof(struct ListNode));
+  l2->val = 5;
+  l2->next = malloc(sizeof(struct ListNode));
+  l2->next->val = 6;
+  l2->next->next = malloc(sizeof(struct ListNode));
+  l2->next->next->val = 4;
+
+  struct ListNode *r = addTwoNumbers(l1, l2);
+  while (r != NULL)
+  {
+    printf("%d ", r->val);
+    r = r->next;
+  }
+  printf("\n");
+}
